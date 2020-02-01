506b4fac4cb713907c1ae3ea7d9e9b8732036894
Mark Dyson <mark.dyson1@outlook.com>
2020-02-01 16:46:38 +0000

    enviroment set

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://md:password1234@cluster0-lkyo8.mongodb.net/tast_manager?retryWrites=true&w=majority'
-
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 mongo = PyMongo(app)
 
 @app.route('/')
